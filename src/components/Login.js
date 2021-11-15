9003ba8641613ddd6182e937f192e5e7b19e7697
Kelvin Nunez <knunezweb@gmail.com>
2021-11-15 16:13:58 -0500

    User

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState(false);
 
-    const [_user,setUser] = useContext(Context);
+    const [user,setUser] = useContext(Context);
 
     const navigate = useNavigate();
 
